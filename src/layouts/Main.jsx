235e321d1907c5ffac7331eade89c0150527248d
Shahadat-Git <noyon560@gmail.com>
2023-06-02 14:08:57 +0600

    footer added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../shared/Header/Header';
+import Footer from '../shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
